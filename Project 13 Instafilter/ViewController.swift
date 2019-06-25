7b0beea690b4785d7c187a69333ce32c6bf17163
Rinalds Domanovs <rinalds.domanovs@gmail.com>
2019-06-25 12:43:04 +0300

    Initial Commit

---
+++ Project 13 Instafilter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project 13 Instafilter
+//
+//  Created by Rinalds Domanovs on 25/06/2019.
+//  Copyright © 2019 Rinalds Domanovs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
